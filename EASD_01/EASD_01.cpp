5af9e8bb5df6b91d9f5bf05b4f0625df12ccd7ff
Fakhri abdillah <fakriggwp@gmail.com>
2023-04-07 10:12:38 +0700

    Add project files.

---
+++ EASD_01/EASD_01.cpp
@@ -0,0 +1,3 @@
+//1. Jelaskan, mengapa suatu algorithma dibutuhkan untuk menyelesaikan suatu masalah.
+//karena aloritma dapat mengurutan instruksi yang terstruktur dan sistematis yang menggambarkan cara menyelesaikan suatu masalah
+//menguraikan masalah yang kompleks menjadi tugas yang lebih sederhana dan terorganisir
