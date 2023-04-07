20112159559ce22549354bb71c4f2a66633d86bd
Fakhri abdillah <fakriggwp@gmail.com>
2023-04-07 10:13:29 +0700

    pertanyaan kedua

--- EASD_01/EASD_01.cpp
+++ EASD_01/EASD_01.cpp
@@ -1,3 +1,6 @@
 //1. Jelaskan, mengapa suatu algorithma dibutuhkan untuk menyelesaikan suatu masalah.
 //karena aloritma dapat mengurutan instruksi yang terstruktur dan sistematis yang menggambarkan cara menyelesaikan suatu masalah
 //menguraikan masalah yang kompleks menjadi tugas yang lebih sederhana dan terorganisir
+
+//2. Dalam algorithma, di klassifikasikan menjadi 2 data struktur. Sebutkan 2 data struktur tersebut
+//Struktur data linear dan Struktur data non-linear
