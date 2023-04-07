5b8423b336d8b06d7d00bc369eb878c3f07957c1
Fakhri abdillah <fakriggwp@gmail.com>
2023-04-07 10:13:51 +0700

    pertanyaan ketiga

--- EASD_01/EASD_01.cpp
+++ EASD_01/EASD_01.cpp
@@ -4,3 +4,6 @@
 
 //2. Dalam algorithma, di klassifikasikan menjadi 2 data struktur. Sebutkan 2 data struktur tersebut
 //Struktur data linear dan Struktur data non-linear
+
+//3. Sebutkan faktor-faktor yang mempengaruhi mempengaruhi efisiensi suatu program algorithma!
+//Kompleksitas algoritma,Struktur data,Bahasa pemrograman,RAM,ukuran data/input
